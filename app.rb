3679fb16cf47df8869d091443b976d7afd88b91d
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-21 03:34:51 +0000

    passed all tests

--- app.rb
+++ app.rb
@@ -23,6 +23,31 @@ end
 
 
 get("/paper") do
+  moves = ["rock", "paper", "scissors"]
 
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
     erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+    erb(:scissors)
 end
